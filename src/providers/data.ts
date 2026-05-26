1f82232b93fc4df2238a5c46593c82f0068e4972
Boluski1 <babatundebolu@rocketmail.com>
2026-05-26 15:54:02 +0100

    implement classes list

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -52,9 +52,15 @@ const options: CreateDataProviderOptions = {
 
             const value = String(filter.value)
 
-            if ( resource === 'subjects') {
+            if (resource === 'subjects') {
               if (field === 'department') params.department = value;
-              if (field === 'name' || field === 'code' ) params.search = value;
+              if (field === 'name' || field === 'code') params.search = value;
+            }
+
+            if (resource === 'classes') {
+              if (field === 'name') params.search = value;
+              if (field === 'subject') params.subject = value;
+              if (field === 'teacher') params.teacher = value;
             }
           })
 
